8263068e373d7d28e6429d988e9c0ec0e699d873
Zack O'Brien <zackobrien18@gmail.com>
2025-12-15 21:07:59 +1000

    Finalised characterBody zombies

--- zombie-game-project-thingy/zombie/zombie.gd
+++ zombie-game-project-thingy/zombie/zombie.gd
@@ -2,18 +2,17 @@ extends CharacterBody3D
 
 @onready var player = get_node("../Player")
 @export var speed : float = 8.0
-@export var player_threshold : float = 2
+@export var player_threshold : float = 3
 
 var previous_position : Vector3
 
 func _physics_process(delta: float) -> void:
 	var player_pos : Vector3 = player.position
 	
-	var direction : Vector3 = (player_pos - self.position)
-	var direction_norm : Vector3 = direction.normalized()
+	var direction : Vector3 = (player_pos + Vector3(randf(), randf(), randf()) - self.position).normalized()
 	
-	velocity.x = direction_norm.x * speed
-	velocity.z = direction_norm.z * speed
+	velocity.x = direction.x * speed + randf_range(-0.5, 0.5)
+	velocity.z = direction.z * speed + randf_range(-0.5, 0.5)
 	
 	if !is_on_floor():
 		velocity.y -= 30 * delta
@@ -24,14 +23,13 @@ func _physics_process(delta: float) -> void:
 	var actual_speed = distance_travelled.length() / delta
 	
 	var touching_player : bool = false
-	if direction.length() < player_threshold:
+	var distance_player = Vector2(player_pos.x - position.x, player_pos.z - position.z)
+	if distance_player.length() < player_threshold:
 		touching_player = true
 	else: 
 		touching_player = false
 	
-	print(touching_player)
 	if speed > 0 && actual_speed < speed * 0.5 && !touching_player:
-		print("stuck")
 		velocity.y = 4
 	
 	previous_position = self.position

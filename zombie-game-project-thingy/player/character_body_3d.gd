2bec941160c5fa0e80d4b7835b89f29d6b20c4f3
Zack O'Brien <zackobrien18@gmail.com>
2025-12-15 14:19:02 +1000

    Reworked movement code

--- zombie-game-project-thingy/player/character_body_3d.gd
+++ zombie-game-project-thingy/player/character_body_3d.gd
@@ -1,36 +1,36 @@
 extends CharacterBody3D
 
-@export var speed : int = 10;
-@export var fall_acceleration : int = 50
-
-var target_velocity : Vector3 = Vector3.ZERO
+@export var acceleration : float = 40
+@export var gravity : float = 30
+@export var jump_force : float = 10
 
 func _physics_process(delta: float) -> void:
 	var direction : Vector3 = Vector3.ZERO
 	
 	if Input.is_action_pressed("forward"):
 		direction.z -= 1.0
-	
 	if Input.is_action_pressed("backward"):
 		direction.z += 1.0
-	
 	if Input.is_action_pressed("left"):
 		direction.x -= 1.0
-	
 	if Input.is_action_pressed("right"):
 		direction.x += 1.0
 	
-	#print(direction)
-	
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
-		#$Pivot.basis = Basis.looking_at(direction)
+		#$Pivot.basis = basis.looking_at(direction)
 	
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	if is_on_floor():
+		velocity.x = direction.x * acceleration
+		velocity.z = direction.z * acceleration
+	else:
+		velocity.x = (direction.x * acceleration) * 0.25
+		velocity.z = (direction.z * acceleration) * 0.25
 	
 	if !is_on_floor():
-		target_velocity.y = direction.y * fall_acceleration
+		velocity.y -= gravity * delta
 	
-	velocity = target_velocity
+	if Input.is_action_pressed("jump") && is_on_floor():
+		velocity.y += jump_force
+	
 	move_and_slide()

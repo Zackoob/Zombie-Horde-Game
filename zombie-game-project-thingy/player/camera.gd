e06b0c2765f34afbc7f88bef4b1b5f69317cd040
Zack O'Brien <zackobrien18@gmail.com>
2025-12-18 19:06:06 +1000

    Mouse capture togglable with esc key

--- zombie-game-project-thingy/player/camera.gd
+++ zombie-game-project-thingy/player/camera.gd
@@ -12,12 +12,17 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion and Input.get_mouse_mode() != 0:
+	if event is InputEventMouseMotion && Input.get_mouse_mode() != 0:
 		yaw += -event.relative.x * yaw_sensitivity
 		pitch += event.relative.y * pitch_sensitivity
+	elif Input.is_action_pressed("menu"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
-	camera_target.rotation.y = lerpf(camera_target.rotation.y, yaw, delta * 10)
-	camera_target.rotation.x = lerpf(camera_target.rotation.x, pitch, delta * 10)
+	camera_target.rotation.y = lerpf(camera_target.rotation.y, yaw, delta * 6)
+	camera_target.rotation.x = lerpf(camera_target.rotation.x, pitch, delta * 6)
 	
 	pitch = clamp(pitch, deg_to_rad(pitch_min), deg_to_rad(pitch_max))

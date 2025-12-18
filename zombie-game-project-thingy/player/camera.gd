dc4cded05564aa80784cb53529084239d77771bf
Zack O'Brien <zackobrien18@gmail.com>
2025-12-18 18:52:01 +1000

    Working third person camera

---
+++ zombie-game-project-thingy/player/camera.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@export var camera_target : Node3D
+@export var pitch_max : float = 25
+@export var pitch_min : float = -50
+var yaw = float()
+var pitch = float()
+var yaw_sensitivity : float = .002
+var pitch_sensitivity : float = .002
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and Input.get_mouse_mode() != 0:
+		yaw += -event.relative.x * yaw_sensitivity
+		pitch += event.relative.y * pitch_sensitivity
+
+func _physics_process(delta: float) -> void:
+	camera_target.rotation.y = lerpf(camera_target.rotation.y, yaw, delta * 10)
+	camera_target.rotation.x = lerpf(camera_target.rotation.x, pitch, delta * 10)
+	
+	pitch = clamp(pitch, deg_to_rad(pitch_min), deg_to_rad(pitch_max))
--- zombie-game-project-thingy/player/character_body_3d.gd
+++ zombie-game-project-thingy/player/character_body_3d.gd
@@ -4,8 +4,17 @@ extends CharacterBody3D
 @export var gravity : float = 30
 @export var jump_force : float = 10
 
+# Camera stuff
+@export var camera_target : Node3D
+@export var camera_parent : Node3D
+var camera_t = float()
+var cam_speed = float()
+
+var camera_direction : Vector3
+
 func _physics_process(delta: float) -> void:
 	var direction : Vector3 = Vector3.ZERO
+	camera_t = camera_target.global_transform.basis.get_euler().y #this might be fine
 	
 	if Input.is_action_pressed("forward"):
 		direction.z -= 1.0
@@ -18,14 +27,20 @@ func _physics_process(delta: float) -> void:
 	
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
-		#$Pivot.basis = basis.looking_at(direction)
+		camera_direction = Vector3(direction.x, 0, direction.z).rotated(Vector3.UP, camera_t).normalized() #this
+		rotation.y = lerp_angle(rotation.y, atan2(-camera_direction.x, -camera_direction.z), delta * acceleration) #this
+	
+	var input_velocity : Vector3
 	
 	if is_on_floor():
-		velocity.x = direction.x * acceleration
-		velocity.z = direction.z * acceleration
+		input_velocity.x = direction.x * acceleration
+		input_velocity.z = direction.z * acceleration
 	else:
-		velocity.x = (direction.x * acceleration) * 0.25
-		velocity.z = (direction.z * acceleration) * 0.25
+		input_velocity.x = (direction.x * acceleration) * 0.25
+		input_velocity.z = (direction.z * acceleration) * 0.25
+	
+	input_velocity = input_velocity.rotated(Vector3.UP, camera_t).normalized() * acceleration
+	velocity = input_velocity
 	
 	if !is_on_floor():
 		velocity.y -= gravity * delta
@@ -34,3 +49,10 @@ func _physics_process(delta: float) -> void:
 		velocity.y += jump_force
 	
 	move_and_slide()
+	camera_smooth_follow(delta)
+
+func camera_smooth_follow(delta):
+	var cam_offset = Vector3(0, 1.5, 0).rotated(Vector3.UP, camera_t)
+	cam_speed = 250
+	var cam_timer = clamp(delta * cam_speed / 20, 0, 1)
+	camera_parent.global_transform.origin = camera_parent.global_transform.origin.lerp(self.global_transform.origin + cam_offset, cam_timer)

61a58e99726be7852ea87c0c5459beb8663e5914
Zack O'Brien <zackobrien18@gmail.com>
2025-12-25 21:50:15 +1000

    Concrete building import

--- zombie-game-project-thingy/zombie/physicsBodyZombie.gd
+++ zombie-game-project-thingy/zombie/physicsBodyZombie.gd
@@ -1,5 +1,6 @@
 extends RigidBody3D
 
+@export var chase_player : bool = true # DEBUG
 @export var player : Node
 @export var speed : float = 8.0
 #@export var speed_threshold : float = 0.25 # % of speed var to determine when zombie should climb
@@ -25,28 +26,29 @@ func _ready() -> void:
 	#$MeshInstance3D.position.y -= ground_threshold # until hovering is fixed mesh is pushed down
 
 func _physics_process(delta: float) -> void:
-	var distance = (player.position - position).length()
-	var direction : Vector3
-	if distance > 1:
-		direction = (player.position + Vector3(randf_range(-20, 20), 0, randf_range(-20, 20)) - position).normalized()
-	else: 
-		direction = (player.position - position).normalized()
-	
-	var force : Vector3 = Vector3.ZERO
-	force.x = direction.x * speed * (clampf(position.y * 0.25, 1.0, 3.0) + clampf(distance / 25, 1.0, 3.0))
-	force.z = direction.z * speed * (clampf(position.y * 0.25, 1.0, 3.0)  + clampf(distance / 25, 1.0, 3.0))
-	
-	#Climbing code
-	if position.y < 6 && (Engine.get_physics_frames() + update_offset) % 4 == 0:
-		check_wall(direction)
-	elif position.y >= 6:
-		is_climbing = false
-		force.y = -200
-	
-	if is_climbing:
-		force.y = 400
-	
-	apply_central_force(force)
+	if chase_player: # DEBUG
+		var distance = (player.position - position).length()
+		var direction : Vector3
+		if distance > 1:
+			direction = (player.position + Vector3(randf_range(-20, 20), 0, randf_range(-20, 20)) - position).normalized()
+		else: 
+			direction = (player.position - position).normalized()
+		
+		var force : Vector3 = Vector3.ZERO
+		force.x = direction.x * speed * (clampf(position.y * 0.25, 1.0, 3.0) + clampf(distance / 25, 1.0, 3.0))
+		force.z = direction.z * speed * (clampf(position.y * 0.25, 1.0, 3.0)  + clampf(distance / 25, 1.0, 3.0))
+		
+		#Climbing code
+		if position.y < 6 && (Engine.get_physics_frames() + update_offset) % 4 == 0:
+			check_wall(direction)
+		elif position.y >= 6:
+			is_climbing = false
+			force.y = -200
+		
+		if is_climbing:
+			force.y = 400
+		
+		apply_central_force(force)
 
 func check_wall(direction):
 	var ray = get_world_3d().direct_space_state 
